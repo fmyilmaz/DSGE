7480497950f2a19b91048907ee6bab7f12a96b31
Fırat Melih Yılmaz <41309751+fmyilmaz@users.noreply.github.com>
2020-09-01 21:33:34 +0300

    version 0.0.1

---
+++ JuliaBasics.jl
@@ -0,0 +1,35 @@
+# to create vector, put comma
+V = [1, 2, 3, 4, 5, 6]
+
+# to create matrix just add space
+V1 = [1 2 3 4 5 6]
+
+# to declare an array just put semicolon
+M1 = [1 2 3; 4 5 6; 7 8 9]
+
+# to resahepe vector or matrix just use resahepe()
+
+V3 = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+reshape(V3, 3,3) # for vector
+
+M2 = [1 2 3; 4 5 6] 
+reshape(M2, 3, 2) # for matrix 2x3 to 3x2
+
+# to create identitiy matirx 
+using LinearAlgebra
+Matrix{Int}(I, 4, 4)
+
+# for dimensions and size of matrix use ndims() and size(), and to see type of elements in matrix using eltype()
+ndims(M2)
+size(M2)
+eltype(M1)
+
+# to concatenate two vector use vcat (similar to cbind() in R) and use hcat similar to (rbind() in R)
+
+d1 = [1 2 3; 4 5 6]
+d2 = [6 7 8; 9 10 11]
+
+hcat(d1, d2)
+vcat(d1, d2)
+
+
